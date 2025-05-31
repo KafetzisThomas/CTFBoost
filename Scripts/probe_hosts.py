01f3b1e6adb38bf0c98743821099244b37857fde
KafetzisThomas <tomkafetzis06@gmail.com>
2025-05-31 11:51:05 +0300

    Add ip check to safely resolve domains

--- Scripts/probe_hosts.py
+++ Scripts/probe_hosts.py
@@ -2,8 +2,16 @@
 # -*- coding: UTF-8 -*-
 
 import requests
+import socket
+import ipaddress
 from .utils import save_results
 
+def is_ip(address):
+    try:
+        ipaddress.ip_address(address)
+        return True
+    except ValueError:
+        return False
 
 def probe_host(target):
     output = []
@@ -14,6 +22,12 @@ def probe_host(target):
         line = f"{target} - {response.status_code}: {response.reason}"
         print(line)
         output.append(line)
+
+        # If target is domain resolve to IP and prompt user to add to /etc/hosts
+        if not is_ip(target):
+            ip = socket.gethostbyname(target)
+            print(f"Add to /etc/hosts: {ip} {target}")
+            input("Press Enter once done...")
     except requests.RequestException:
         line = f"{target} - Unreachable"
         print(line)
