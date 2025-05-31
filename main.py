ce37c874905b19b28dc1866a0dd852a60e7cfea4
KafetzisThomas <tomkafetzis06@gmail.com>
2025-05-31 12:42:09 +0300

    Replace sys.argv with argparse

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # NOTE: By contributing to this project, you agree to the terms of the GPLv3 license, and agree to grant the project owner the right to also provide or sell this software, including your contribution, to anyone under any other license, with no compensation to you.
 
 import sys
+import argparse
 import colorama
 from colorama import Fore as F
 from Scripts.probe_hosts import probe_host
@@ -32,30 +33,49 @@ By KafetzisThomas
 def main():
     print(banner)
     print("---" * 28)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", help="target host or ip address")
+    parser.add_argument("--probe", action="store_true", help="probe the host")
+    parser.add_argument(
+        "--dnsenum", action="store_true", help="perform dns enumeration"
+    )
+    parser.add_argument(
+        "--quicknmap", action="store_true", help="run a quick nmap scan"
+    )
+    parser.add_argument("--fullnmap", action="store_true", help="run a full nmap scan")
+    parser.add_argument(
+        "--ffufdir", action="store_true", help="perform directory fuzzing with ffuf"
+    )
+    parser.add_argument(
+        "--ffufsub", action="store_true", help="perform subdomain fuzzing with ffuf"
+    )
+
+    args = parser.parse_args()
     output = ""
 
     # Host probing
-    if "probe" in sys.argv:
+    if args.probe:
         probe_host(sys.argv[1])
 
     # DNS enumeration
-    if "dnsenum" in sys.argv:
+    if args.dnsenum:
         dns_enumeration(sys.argv[1])
 
     # Nmap
-    if "quicknmap" in sys.argv:
+    if args.quicknmap:
         check_nmap()
         output = quicknmap(target=sys.argv[1])
-    elif "fullnmap" in sys.argv:
+    elif args.fullnmap:
         check_nmap()
         output = fullnmap(target=sys.argv[1])
 
     # fuff
     if detect_web_service(output):
-        if "ffufdir" in sys.argv:
+        if args.ffufdir:
             check_ffuf()
             directory_fuzzing(target=sys.argv[1])
-        elif "ffufsub" in sys.argv:
+        elif args.ffufsub:
             check_ffuf()
             subdomain_fuzzing(target=sys.argv[1])
 
